ecbbf336be68c3d20f5bfc7cee82684c4aa17bc8
woziji <woziji@163.com>
2022-04-19 11:08:33 +0800

    first commit

---
+++ src/main/java/com/wang/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.wang.git;
+
+/**
+ * @ClassName: GitTest
+ * @Description
+ * @Author:我自己
+ * @Date: 2022/4/19  11:04
+ * @Version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
