f3c3e9f91bf4bbbc53c6c4785f462aadb85e22b0
woziji <woziji@163.com>
2022-04-19 14:06:20 +0800

    push commit

--- src/main/java/com/wang/git/GitTest.java
+++ src/main/java/com/wang/git/GitTest.java
@@ -15,5 +15,6 @@ public class GitTest {
         System.out.println("Hello Git4!");
         System.out.println("Hello Git5!");
         System.out.println("Hello test!");
+        System.out.println("push test!");
     }
 }
